868791490aa2ab5570f02874fedf54f509fba2a2
rayliu7717 <rayliu2004@gmail.com>
2024-06-30 09:34:16 -0400

    init canvas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import {useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function App() {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    contextRef.current = context;
+    const game = {grid:40};
+    document.body.prepend(canvas);
+    canvas.setAttribute('width',game.grid*15);
+    canvas.setAttribute('height',game.grid*10);
+    canvas.style.border = '1px solid black';
+  },[])
+
+  const onMouseDown = ({nativeEvent}) => {
+    let {x,y} = nativeEvent;
+    nativeEvent.preventDefault();
+  };
+  const onMouseUp = ({nativeEvent}) => {
+    let {x,y} = nativeEvent;
+    nativeEvent.preventDefault();
+  };
+  const onMouseMove = ({nativeEvent}) => {
+    let {x,y} = nativeEvent;
+    nativeEvent.preventDefault();
+  };
+  const onMouseLeave = ({nativeEvent}) => {
+    let {x,y} = nativeEvent;
+    nativeEvent.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas className="canvas-container"
+        width={800}
+        height={600}
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+        onMouseUp={onMouseUp}
+        onMouseLeave={onMouseLeave}
+      >
+      </canvas>
     </div>
   );
 }
