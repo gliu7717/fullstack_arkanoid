cf88145b1299320fb75f6fa2981f5709c80cd4eb
rayliu7717 <rayliu2004@gmail.com>
2024-06-30 11:28:11 -0400

    add ball and ball move

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
   const game = {grid:60,ani:''};
+  const ball = {x:game.grid*7,y:game.grid*5,w:game.grid/3,h:game.grid/3,color:'green',dx:5,dy:5};
   const player = {x:game.grid*7,y:game.grid*8,w:game.grid*2,h:game.grid/2,color:'red',speed:5};
   const keyz = {ArrowLeft:false,ArrowRight:false};
   
@@ -32,18 +33,48 @@ function App() {
     if(keyz.ArrowLeft){player.x-= player.speed;}
     if(keyz.ArrowRight){player.x+= player.speed;}
   }
-  
-  function draw(){
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
 
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    movement();
+  function drawPlayer(ctx)
+  {
     ctx.beginPath();
     ctx.rect(player.x,player.y,player.w,player.h);
     ctx.fillStyle = player.color;
     ctx.fill();
     ctx.closePath();
+  }
+
+  function ballmove(canvas){
+    if(ball.x > canvas.width || ball.x < 0  ){
+      ball.dx *= -1;
+    }
+    if(ball.y > canvas.height || ball.y < 0 ){
+      ball.dy *= -1;
+    }
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+  }
+  function drawBall(ctx){
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.rect(ball.x,ball.y,ball.w,ball.h);
+    ctx.stroke();
+    ctx.closePath();    
+    ctx.beginPath();
+    ctx.fillStyle = ball.color;
+    let adj = ball.w/2;
+    ctx.arc(ball.x+adj,ball.y+adj,ball.w/2,0,Math.PI*2);
+    ctx.fill();
+    ctx.closePath();    
+  }
+
+  function draw(){
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    movement();
+    ballmove(canvas)
+    drawPlayer(ctx);
+    drawBall(ctx);
     game.ani = requestAnimationFrame(draw);
   }
 
